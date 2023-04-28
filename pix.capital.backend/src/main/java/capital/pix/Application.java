9e42d8d7e10c89fdee5a1f6447d2742713dcead9
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-04-28 16:00:33 -0300

    fix

--- pix.capital.backend/src/main/java/capital/pix/Application.java
+++ pix.capital.backend/src/main/java/capital/pix/Application.java
@@ -1,4 +1,4 @@
-package capital.pix.pix.capital.backend;
+package capital.pix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
