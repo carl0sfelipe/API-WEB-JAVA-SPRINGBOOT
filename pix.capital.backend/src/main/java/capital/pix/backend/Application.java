7165842f2f087f1a44540a6243e1d4b8350a267e
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-04-28 16:41:28 -0300

    bug

--- pix.capital.backend/src/main/java/capital/pix/backend/Application.java
+++ pix.capital.backend/src/main/java/capital/pix/backend/Application.java
@@ -2,13 +2,14 @@ package capital.pix.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "capital.pix.backend.repository")
+@EntityScan(basePackages = "capital.pix.backend.model")
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
