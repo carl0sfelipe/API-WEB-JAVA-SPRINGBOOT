801e34d0d1459cee1ce2377e348375f16511cb1a
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-04-28 16:13:27 -0300

    fix

---
+++ pix.capital.backend/src/main/java/capital/pix/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package capital.pix.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
