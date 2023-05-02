0ee03c5e1c1967b816aaa14e4c14648c58ce76ef
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-05-02 15:37:32 -0300

    del

--- src/main/java/capital/pix/repository/ClienteRepo.java
+++
@@ -1,8 +0,0 @@
-package capital.pix.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import capital.pix.models.Cliente;
-
-public interface ClienteRepo extends CrudRepository <Cliente, Integer> {
-    
-}
