c4cc89ac7289503de88eebc52e3e65a5b1465a80
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-05-04 00:13:45 -0300

    estilizaçao

--- frontend/banco/src/components/CriarAgencia.js
+++ frontend/banco/src/components/CriarAgencia.js
@@ -1,71 +1,99 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './CreateAgency.css';
 
 const CreateAgency = () => {
-    const [nome, setNome] = useState('');
-    const [endereco, setEndereco] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [message, setMessage] = useState('');
+  const [nome, setNome] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [message, setMessage] = useState('');
+  const [showForm, setShowForm] = useState(false);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            const response = await axios.post('http://localhost:8080/agencia', {
-                nome,
-                endereco,
-                telefone,
-            });
+    try {
+      const response = await axios.post('http://localhost:8080/agencia', {
+        nome,
+        endereco,
+        telefone,
+      });
 
-            if (response.status === 201) {
-                setMessage('Agência criada com sucesso!');
-            } else {
-                setMessage('Erro ao criar agência. Tente novamente.');
-            }
-        } catch (error) {
-            setMessage('Erro ao criar agência. Tente novamente.');
-            console.log(error);
-        }
-    };
+      if (response.status === 201) {
+        setMessage('Agência criada com sucesso!');
+      } else {
+        setMessage('Erro ao criar agência. Tente novamente.');
+      }
+    } catch (error) {
+      setMessage('Erro ao criar agência. Tente novamente.');
+      console.log(error);
+    }
+  };
 
-    return (
-        <div>
-            <h2>Criar Agência</h2>
-            <form onSubmit={handleSubmit}>
-          
+  const handleButtonClick = () => {
+    setShowForm(true);
+  };
 
-                <label htmlFor="nome">Nome:</label>
-                <input
-                    type="text"
-                    id="nome"
-                    value={nome}
-                    onChange={(e) => setNome(e.target.value)}
-                />
-                <br />
+  const handleCloseButtonClick = () => {
+    setShowForm(false);
+    setMessage('');
+    setNome('');
+    setEndereco('');
+    setTelefone('');
+  };
 
-                <label htmlFor="endereco">Endereço:</label>
-                <input
-                    type="text"
-                    id="endereco"
-                    value={endereco}
-                    onChange={(e) => setEndereco(e.target.value)}
-                />
-                <br />
+  return (
+    <div>
+      {showForm ? (
+        <div className="create-agency-form">
+          <h2>Criar Agência</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="nome">Nome:</label>
+              <input
+                type="text"
+                id="nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+            </div>
 
-                <label htmlFor="telefone">Telefone:</label>
-                <input
-                    type="text"
-                    id="telefone"
-                    value={telefone}
-                    onChange={(e) => setTelefone(e.target.value)}
-                />
-                <br />
+            <div className="form-group">
+              <label htmlFor="endereco">Endereço:</label>
+              <input
+                type="text"
+                id="endereco"
+                value={endereco}
+                onChange={(e) => setEndereco(e.target.value)}
+              />
+            </div>
 
-                <button type="submit">Criar Agência</button>
-            </form>
-            {message && <p>{message}</p>}
+            <div className="form-group">
+              <label htmlFor="telefone">Telefone:</label>
+              <input
+                type="text"
+                id="telefone"
+                value={telefone}
+                onChange={(e) => setTelefone(e.target.value)}
+              />
+            </div>
+
+            <div className="form-actions">
+              <button type="submit">Criar Agência</button>
+              <button className="cancel-button" onClick={handleCloseButtonClick}>
+                Fechar
+              </button>
+            </div>
+          </form>
         </div>
-    );
+      ) : (
+        <button className="create-agency-button" onClick={handleButtonClick}>
+          Criar Agência
+        </button>
+      )}
+      {message && <p>{message}</p>}
+    </div>
+  );
 };
 
 export default CreateAgency;
