66e811108d8347d52e44996c0f520a5ee3493fb9
Carlos Macbook Pro <carlosmac@Carloss-MacBook-Pro.local>
2023-05-04 23:13:30 -0300

    bem-vindo

--- frontend/banco/src/components/Bankline.js
+++ frontend/banco/src/components/Bankline.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Depositar from './Depositar';
 import Sacar from './Sacar';
 import Transferir from './Transferir';
 import ExibirExtrato from './ExibirExtrato';
 import './CreateAgency.css';
 
-
 const Bankline = () => {
   const { id } = useParams();
-  console.log("id"+id)
+  const [clientName, setClientName] = useState('');
+
+  useEffect(() => {
+    const fetchClient = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/cliente/${id}`);
+        setClientName(response.data.nome);
+      } catch (error) {
+        console.error('Erro ao buscar cliente:', error);
+      }
+    };
+
+    fetchClient();
+  }, [id]);
 
   return (
-    <div className="create-agency-form" >
+    <div className="create-agency-form">
       <h2>Bankline</h2>
-      <h3>Cliente ID: {id}</h3>
+      {clientName && <h3>Bem-vindo, {clientName}</h3>}
       <Depositar clienteId={id} />
-       <Sacar clienteId={id} />
+      <Sacar clienteId={id} />
       <Transferir clienteId={id} />
-      <ExibirExtrato clienteId={id}/>
+      <ExibirExtrato clienteId={id} />
     </div>
   );
 };
